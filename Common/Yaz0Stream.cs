073eee4bede23140270e0a1fd65633551560ae1f
Matt Stevens <matt@handsomematt.co.uk>
2017-04-28 00:47:17 +0100

    fix me for fucks sake joseph

--- Common/Yaz0Stream.cs
+++ Common/Yaz0Stream.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -45,11 +46,78 @@ namespace BotWLib.Common
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            // todo: this NEEDS to decompress bytes
-            // but also not advance the stream too far ahead!
-            // say if we have a string ABCCCCC and we read 4 bytes of: ABCC
-            // we don't want the stream to be at the ned of ABCCCC*, we want it at ABCC*CC
-            return compressedStream.Read(buffer, offset, count);
+            if (DecompressEnumerator == null)
+                DecompressEnumerator = Decompress(compressedStream).GetEnumerator();
+
+            int i;
+            for (i = 0; i < count; i++)
+            {
+                DecompressEnumerator.MoveNext();
+                buffer[offset + i] = DecompressEnumerator.Current;
+            }
+
+            return i;
+        }
+
+        private IEnumerator<byte> DecompressEnumerator;
+
+        private IEnumerable<byte> Decompress(Stream input)
+        {
+            // Decompress the data.
+            int decompressedBytes = 0;
+            while (decompressedBytes < decompressedLength)
+            {
+                // Read the configuration byte of a decompression setting group, and go through each bit of it.
+                byte groupConfig = (byte)input.ReadByte();
+                for (int i = 7; i >= 0; i--)
+                {
+                    // Check if bit of the current chunk is set.
+                    if ((groupConfig & (1 << i)) == (1 << i))
+                    {
+                        // Bit is set, copy 1 raw byte to the output.
+                        yield return (byte)input.ReadByte();
+                        decompressedBytes++;
+                    }
+                    else if (decompressedBytes < decompressedLength) // This does not make sense for last byte.
+                    {
+                        // Bit is not set and data copying configuration follows, either 2 or 3 bytes long.
+                        byte byte1 = (byte)input.ReadByte();
+                        byte byte2 = (byte)input.ReadByte();
+                        ushort dataBackSeekOffset = (ushort)(((byte1 & 0xF) << 8) | byte2);
+                        int dataSize;
+                        // If the nibble of the first back seek offset byte is 0, the config is 3 bytes long.
+                        byte nibble = (byte)(dataBackSeekOffset >> 12/*1 byte (8 bits) + 1 nibble (4 bits)*/);
+                        if (nibble == 0)
+                        {
+                            // Nibble is 0, the number of bytes to read is in third byte, which is (size + 0x12).
+                            dataSize = input.ReadByte() + 0x12;
+                        }
+                        else
+                        {
+                            // Nibble is not 0, and determines (size + 0x02) of bytes to read.
+                            dataSize = nibble + 0x02;
+                            // Remaining bits are the real back seek offset.
+                            dataBackSeekOffset &= 0x0FFF;
+                        }
+                        // Since bytes can be reread right after they were written, write and read bytes one by one.
+                        for (int j = 0; j < dataSize; j++)
+                        {
+                            // Read one byte from the current back seek position.
+                            //writer.BaseStream.Position -= dataBackSeekOffset + 1;
+                            //byte readByte = (byte)writer.BaseStream.ReadByte();
+                            // Write the byte to the end of the memory stream.
+                            //writer.Seek(0, SeekOrigin.End);
+                            //writer.Write(readByte);
+
+                            byte readByte = 0x21;
+
+                            yield return readByte;
+
+                            decompressedBytes++;
+                        }
+                    }
+                }
+            }
         }
 
         public override bool CanRead
